2d523469f72f45fc751bdf647214b02b699194dc
tbeben77 <tbeben77@gmail.com>
2020-12-25 23:52:22 +0900

    [자바의 정석 - 기초편] ch5-18~20 2차원 배열

---
+++ src/ch05/ch5_18_20.java
@@ -0,0 +1,16 @@
+package ch05;
+
+import java.util.Arrays;
+
+public class ch5_18_20 {
+
+	public static void main(String[] args) {
+		int[][] score = {
+				{100, 100, 100},
+				{20, 20, 20},
+				{30, 30, 30},
+				{40, 40, 40}
+		};
+	}
+
+}
