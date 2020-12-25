f9d1b1479d3eae187cbb001d0bee2ded3947c22e
tbeben77 <tbeben77@gmail.com>
2020-12-25 23:55:39 +0900

    2차원배열 for문 한번으로 출력하기

--- src/ch05/ch5_18_20.java
+++ src/ch05/ch5_18_20.java
@@ -11,6 +11,17 @@ public class ch5_18_20 {
 				{30, 30, 30},
 				{40, 40, 40}
 		};
+		//2차원배열 for문 한번으로 출력하기
+		 for (int i = 0; i < score.length * score[0].length; i++) {
+	            int row = i / score[0].length; // 행
+	            int column = i % score[0].length; // 열
+	            System.out.print(score[row][column] + "\t");
+	 
+	 
+	            if (column == score[0].length - 1) {
+	                System.out.println();
+	            }
+	        }
 	}
 
 }
