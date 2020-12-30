abceeddd3510790eba593dc08b5f89aa80f0d5fc
tbeben77 <tbeben77@gmail.com>
2020-12-30 23:13:03 +0900

    [자바의 정석 - 기초편] ch6-11 선언위치 변수종류

---
+++ src/ch06/ch6_11.java
@@ -0,0 +1,11 @@
+package ch06;
+
+public class ch6_11 {
+
+	int iv;			// 인스턴스 변수	-> 클래스영역
+	static int cv;	// 클래스 변수(static변수, 공유변수)
+	
+	public static void main(String[] args) {
+		int lv = 0;	// 지역변수 -> 메서드영역
+	}
+}
