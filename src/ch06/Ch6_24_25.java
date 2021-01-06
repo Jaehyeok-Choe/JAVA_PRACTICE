4acee9e4b4b2eb84f5ffdd49b5001b5cda6532ba
tbeben77 <tbeben77@gmail.com>
2021-01-07 00:06:15 +0900

    [자바의 정석 - 기초편] ch6-24,25 참조형매개변수, 참조형 반환타입

--- src/ch06/Ch6_24_25.java
+++ src/ch06/Ch6_24_25.java
@@ -4,21 +4,38 @@ public class Ch6_24_25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Data2 d = new Data2();
-		d.x = 10;
-		System.out.println("main() : x = " + d.x);
+		/*
+		 * Data2 d = new Data2(); d.x = 10; System.out.println("main() : x = " + d.x);
+		 * 
+		 * change(d); System.out.println("After change(d)");
+		 * System.out.println("main() : x = " + d.x);
+		 */
 		
-		change(d);
-		System.out.println("After change(d)");
-		System.out.println("main() : x = " + d.x);
+		Data3 d = new Data3();
+		d.x = 10;
+		
+		Data3 d2 = copy(d);
+		System.out.println("d2.x = " + d.x);
+		System.out.println("d3.x = " + d2.x);
 	}
+	
 	
 	static void change(Data2 d) {	// 참조형 매개변수
 		d.x = 1000;
 		System.out.println("change() : x + " + d.x);
 	}
+	
+	static Data3 copy(Data3 d) {	// 참조형 반환타입
+		Data3 tmp = new Data3();
+		tmp.x = d.x;
+		return tmp;
+	}
 }
 
 class Data2{
+	int x;
+}
+
+class Data3{
 	int x;
 }
