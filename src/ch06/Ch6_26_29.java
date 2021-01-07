fb13cfb2e636cd9bf4588fd3d65f632c271d88b3
tbeben77 <tbeben77@gmail.com>
2021-01-07 23:58:55 +0900

    [자바의 정석 - 기초편] ch6-26~29 static메서드와 인스턴스 메서드

---
+++ src/ch06/Ch6_26_29.java
@@ -0,0 +1,27 @@
+package ch06;
+
+public class Ch6_26_29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MyMath3.add(200L, 100L));	// 클래스메서드 호출
+		MyMath3 mm = new MyMath3();	// 인스턴스 생성
+		mm.a = 200L;
+		mm.b = 100L;
+		System.out.println(mm.add());	// 인스턴스메서드 호출
+	}
+
+}
+
+class MyMath3{
+	long a, b;
+	
+	long add() {	// 인스턴스메서드
+		return a + b;
+	}
+	
+	static long add(long a, long b) {	// 클래스메서드(static 메서드)
+		return a + b;
+	}
+	
+}
