1f98903dc786113c3657e7d27b3d3455f50779e5
tbeben77 <tbeben77@gmail.com>
2021-01-14 23:02:29 +0900

    [자바의 정석 - 기초편] ch7-21 접근제어자(접근제어자 관련 주석추가)

--- src/ch07/Ch7_21.java
+++ src/ch07/Ch7_21.java
@@ -1,5 +1,14 @@
 package ch07;
 
+/*접근 제어자(access modifier)
+ * 
+ *  private    같은 클래스 내에서만 접근이 가능하다.
+ *  default    같은 패키지 내에서만 접근이 가능하다.
+ *  protected  같은 패키지 내에서, 그리고 다른 패키지의 자손클래스에서 접근이 가능하다.
+ *  public     접근 제한이 전혀 없다.
+ *  
+ * */
+
 class MyParent{
 	private 	int prv;	// 같은 클래스
 				int dft;	// 같은 패키지
