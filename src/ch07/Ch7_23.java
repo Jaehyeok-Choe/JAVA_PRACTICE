48a8e60b8ee73e012fa51f6710740ae938f09c6f
tbeben77 <tbeben77@gmail.com>
2021-01-15 21:50:34 +0900

    [자바의 정석 - 기초편] ch7-23 다형성

---
+++ src/ch07/Ch7_23.java
@@ -0,0 +1,25 @@
+package ch07;
+/*다형성(polymorphism)
+ * 
+ * Q. 참조변수의 타입은 인스턴스의 타입과 반드시 일치해야 하나요?
+ * A. 아닙니다. 일치하는 것이 보통이지만 일치 하지 않을 수도 있습니다. 
+ * 
+ * Q. 참조변수가 조상타입일 때와 자손타입일 때의 차이?
+ * A. 참조변수로 사용할 수 있는 멤버의 갯수가 달라집니다. 
+ * 
+ * Q. 자손 타입의 참조변수로 조상 타입의 객체를 가리킬 수 있나요?
+ * A. 아니요. 허용되지 않습니다. 
+ * 
+ * 	Tv		t = new SmartTv();  // OK. 허용
+ *  SmartTv s = new Tv();		// 에러. 허용 안 됨. 
+ * 
+ * */
+
+public class Ch7_23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
