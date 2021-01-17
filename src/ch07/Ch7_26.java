6480adb18eb5be9cfc57b2695cab7e3ed10beede
tbeben77 <tbeben77@gmail.com>
2021-01-17 21:28:37 +0900

    [자바의 정석 - 기초편] ch7-26 instanceof 연산자

---
+++ src/ch07/Ch7_26.java
@@ -0,0 +1,23 @@
+package ch07;
+
+/*instanceof 연산자
+ * 
+ *  - 참조변수의 형변환 가능여부 확인에 사용. 가능하면 true 반환
+ *  - 형변환 전에 반드시 instanceof로 확인해야 함.
+ *  
+ *  void doWork(Car c){
+ *  	if(c instanceof FireEngine){		// 1. 형변환이 가능한지 확인 
+ *  		FireEngine fe = (FireEngine)c;	// 2. 형변환
+ *  		fe.water();
+ *  	}
+ *  }
+ * */
+
+public class Ch7_26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
